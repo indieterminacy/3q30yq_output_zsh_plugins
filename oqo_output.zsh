d95ed7f2a94101b31bc3c4813c3808506d602240
indieterminacy <indieterminacy@nortonspeal.com>
2016-10-07 14:29:32 +0200

    Committing repo for oq_compton config

---
+++ oqo_output.zsh
@@ -0,0 +1,16 @@
+#Support for conveniently working with Unicode emoji in Zsh.
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/emoji
+#https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/emotty
+#
+# https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/colored-man-pages
+#
+#  A plugin for the zsh syntax highlighter that turns URLs green if they
+#  respond with a "good" status, and red otherwise. Useful for checking URL
+#  typos.
+# https://github.com/ascii-soup/zsh-url-highlighter
+zplug "ascii-soup/zsh-url-highlighter"
+
+# almostontop zsh plugin.
+# Clears previous command output
+# This happens every time before new command executed in shell
+# zplug "Valiev/almostontop"
